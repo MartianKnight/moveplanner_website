36cdca798020e8aa1b3221cf90be2caee63bbbbf
MartianKnight <MartianCastle@gmail.com>
2014-11-13 06:54:46 -0500

    Add NYT API key

--- js/script.js
+++ js/script.js
@@ -11,13 +11,17 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var $street = $('#street');
-    var $city = $('#city');
-    console.log($('#street').val());
-    var HTMLgoogleMapURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location= %street%, %city%';
-    var formattedURL = HTMLgoogleMapURL.replace("%street%", $street.val());
-    formattedURL = formattedURL.replace("%city%", $city.val());
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street + ', ' + city;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var HTMLgoogleMapURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location= %data%';
+    var formattedURL = HTMLgoogleMapURL.replace("%data%", address);
     console.log(formattedURL);
+
+    // NYT API key: 4a9f8aaddf77876bf57d1d02b5c3cde0:19:70181836
 
     var HTMLimage = "<img src='%data%' class='bgimg'>";
     var formattedImage = HTMLimage.replace("%data%", formattedURL);
@@ -25,6 +29,7 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    $.getJSON();
 
     return false;
 };
